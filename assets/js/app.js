be83281162aa4a1a0faf98ec570ea33b423b4151
sanjay <84283520+lsanjayl@users.noreply.github.com>
2022-10-07 07:19:42 +0530

    final

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,6 @@
+// const express = require('express')
+// const axios = require('axios')
+// const app = express()
 const contactForm = document.querySelector('.co-form');
 let name = document.getElementById('name');
 let email = document.getElementById('email');
@@ -6,38 +9,64 @@ let department = document.getElementById('department');
 let collegename = document.getElementById('collegename');
 let currentyear = document.getElementById('currentyear');
 let message = document.getElementById('message');
-if(contactForm){
-contactForm.addEventListener('click',(e)=>{
-    e.preventDefault();
-    let formData = {
-        name: name.value,
-        email:email.value,
-        phone:phone.value,
-        department:department.value,
-        collegename:collegename.value,
-        currentyear:currentyear.value,
-        message:message.value
-    }
-    console.log(formData)
-    let xhr =new XMLHttpRequest();
-    xhr.open('POST','https://creatorverse.in/contact');
-    xhr.setRequestHeader('content-type','application/json');
-    xhr.onload = function(){
-        console.log(xhr.responseText);
-        if(xhr.responseText=='success'){
-            alert('Email sent');
-            name.value='';
-            email.value='';
-            phone.value='';
-            department.value='';
-            collegename.value='';
-            currentyear.value='';
-            message.value='';
+if (contactForm) {
+    contactForm.addEventListener('click', (e) => {
+        e.preventDefault();
+        let formData = {
+            name: name.value,
+            email: email.value,
+            phone: phone.value,
+            department: department.value,
+            collegename: collegename.value,
+            currentyear: currentyear.value,
+            message: message.value,
         }
-        else{
-            alert("error");
+        console.log(formData);
+        // let xhr = new XMLHttpRequest();
+        // xhr.open('POST', 'http://localhost:5000/');
+        // xhr.setRequestHeader('content-type', 'application/json');
+        // xhr.onload = function () {
+        //     console.log(xhr.responseText);
+        //     if (xhr.responseText == 'success') {
+        //         alert('Email sent');
+        //         name.value = '';
+        //         email.value = '';
+        //         phone.value = '';
+        //         department.value = '';
+        //         collegename.value = '';
+        //         currentyear.value = '';
+        //         message.value = '';
+        //     }
+        //     else {
+        //         alert("error");
+        //     }
+        // }
+        // xhr.send(JSON.stringify(formData))
+        const sendHttpsRequest = (method,url,data)=>{
+            const promise = new Promise((resolve,reject)=>{
+                const xhr = new  XMLHttpRequest();
+                xhr.open(method,url);
+                xhr.responseType='json';
+                if(data){
+                    xhr.setRequestHeader('content-type', 'application/json');
+                }
+                xhr.onload=()=>{
+                    resolve(xhr.response)
+                }
+                xhr.send(JSON.stringify(data));
+            })
+            return promise;
         }
-    }
-    xhr.send(JSON.stringify(formData))
-})
+            sendHttpsRequest('POST','https://creatoverse.herokuapp.com/',formData).then(responseData=>{
+                alert('Email sent');
+                name.value = '';
+                email.value = '';
+                phone.value = '';
+                department.value = '';
+                collegename.value = '';
+                currentyear.value = '';
+                message.value = '';
+            });
+        
+    })
 }
