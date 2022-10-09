f4d5fc90357a5b970d4eadffa5079dfd3342c4ce
sanjay <84283520+lsanjayl@users.noreply.github.com>
2022-10-10 02:30:05 +0530

    completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,22 +3,23 @@
 // const app = express()
 const contactForm = document.querySelector('.co-form');
 let name = document.getElementById('name');
-let email = document.getElementById('email');
 let phone = document.getElementById('phone');
 let department = document.getElementById('department');
-let collegename = document.getElementById('collegename');
-let currentyear = document.getElementById('currentyear');
+let Projectrunningstatus=document.getElementById('Projectrunningstatus');
+let finalyear = document.getElementById('finalyear');
+let time = document.getElementById('time');
 let message = document.getElementById('message');
 if (contactForm) {
     contactForm.addEventListener('click', (e) => {
         e.preventDefault();
         let formData = {
             name: name.value,
-            email: email.value,
             phone: phone.value,
+            collegename:collegename.value,
+            Projectrunningstatus:Projectrunningstatus.value,
+            finalyear:finalyear.value,
             department: department.value,
-            collegename: collegename.value,
-            currentyear: currentyear.value,
+            time:time.value,
             message: message.value,
         }
         console.log(formData);
@@ -60,12 +61,13 @@ if (contactForm) {
             sendHttpsRequest('POST','https://creatoverse.herokuapp.com/',formData).then(responseData=>{
                 alert('Email sent');
                 name.value = '';
-                email.value = '';
                 phone.value = '';
                 department.value = '';
                 collegename.value = '';
-                currentyear.value = '';
                 message.value = '';
+                time.value='';
+                finalyear.value='';
+                Projectrunningstatus.value='';
             });
         
     })
